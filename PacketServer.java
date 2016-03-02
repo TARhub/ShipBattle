1db985be6bd9f1762e084197cb70a20135aa9b5a
TARhub <tar.mail2013@gmail.com>
2016-03-01 21:09:19 -0600

    Ready to implement PacketClient and PacketServer
    I will be able to implement it once I fix the Board, so instead of
    hitting yourself, you hit the other player.
    
    I also added javadoc to the PacketServer/EchoThread file.

--- PacketServer.java
+++ PacketServer.java
@@ -1,19 +1,80 @@
 import java.io.*;
 import java.net.*;
 
-public class PacketServer extends Thread {
+/**
+ * Redirects packets to clients via a LAN server.
+ *
+ * @version %I%
+ * @since 1.3
+ */
+public class PacketServer {
     private ServerSocket serverSocket;
+    private Socket socket = null;
     private final int    PORT;
 
-    public PacketServer(int port) {
+    private EchoThread echoThread;
+
+    /**
+     * Constructs a <code>PacketServer</code> object using a port.
+     * This constructor allows for multiple clients, using an infinite
+     * while loop to do so.
+     *
+     * @param port The port # of the server.
+     */
+    public PacketServer(int port) throws IOException {
         this.PORT = port;
         serverSocket = new ServerSocket(PORT);
         serverSocket.setSoTimeout(15000);
+
+
+        while(true) {
+            socket = serverSocket.accept();
+            echoThread = new EchoThread(socket);
+            echoThread.start();
+        }
     }
 
-    public void run() throws IOException {
-        while(true) {
-            Socket acc = serverSocket.accept();
+    /**
+     * Thread for the PacketServer, which recieves packets and immediately
+     * sends it to the other client.
+     *
+     * @version %I%
+     * @since 1.3
+     */
+    public class EchoThread extends Thread {
+        private Socket clientSocket;
+
+        /**
+         * Constructor for the <code>EchoThread</code>.
+         *
+         * @param clientSocket Socket from a client that will send and
+         * recieve packets
+         */
+        public EchoThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            InputStream inFromClient  = null;
+            BufferedReader in         = null;
+            DataOutputStream toClient = null;
+
+            try {
+                inFromClient = clientSocket.getInputStream();
+                in = new BufferedReader(new InputStreamReader(inFromClient));
+                toClient = new DataOutputStream(clientSocket.getOutputStream());
+            } catch (IOException e) {
+                System.err.println("Well, this happened: "+e);
+            }
+
+            try {
+                String txt = in.readLine();
+                toClient.writeUTF(txt);
+                toClient.flush();
+            } catch (IOException e) {
+                System.err.println("Well, this happened: "+e);
+            }
         }
     }
 }
