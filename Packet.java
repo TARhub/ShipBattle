54c9a03db387e699e26752adf109322291c5f1a3
Thomas R <tar.mail2013@gmail.com>
2016-03-16 14:50:01 -0500

    Create Packet.java
    This way, it will be easier to send packets.

---
+++ Packet.java
@@ -0,0 +1,9 @@
+public class Packet {
+    String packet;
+    int player;
+    
+    public Packet(String packet, int player) {
+        this.packet = packet;
+        this.player = player;
+    }
+}
